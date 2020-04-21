4369d8c34a76fa3247124170c30a0dfe0962a87e
vicnx <andanivicente@gmail.com>
2020-04-21 19:37:21 +0200

    gitignore1

--- modules/client/view/js/APIS.js
+++
@@ -1,4 +0,0 @@
-var Apis ={
-    maps: "https://maps.googleapis.com/maps/api/js?key=AIzaSyClXM2-qxMMHYesj4T7Qer4DeRzSIcaiJ4&callback=initMap",
-    news: "http://newsapi.org/v2/everything?q=tablets&apiKey=41e9faf8a62b44c587ba92813cfa3b04"
-} ;
